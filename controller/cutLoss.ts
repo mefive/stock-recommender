9264777c946f5d5c723eaa0213499d544c8b0e66
liucong211463 <liucong211463@sogou-inc.com>
2017-11-23 21:06:55 +0800

    fix

--- controller/cutLoss.ts
+++ controller/cutLoss.ts
@@ -6,14 +6,15 @@ const router = express.Router();
 
 router.get('/api/cutLoss', async (req, res, next) => {
   const {
-    symbol, days = 20, start = moment().subtract(-20).format('YYYY-MM-DD'),
+    symbol, days = 20, start,
   } = req.query;
 
-  const durationDays = moment.duration(moment() - moment(start)).asDays();
+  if (!start) {
+    throw 'symbol must be provided';
+  }
 
-  const datalen = Math.ceil(
-    Math.max(days, durationDays)
-  );
+  // fetch 20 more days to calculate ATR20
+  const datalen = moment.duration(moment() - moment(start)).asDays();
 
   try {
     if (!symbol) {
@@ -33,43 +34,38 @@ router.get('/api/cutLoss', async (req, res, next) => {
       throw 'no data';
     }
 
-
-    if (durationDays > days) {
-      const startIndex = data.findIndex(({ day }) => moment(day) >= moment(start));
-      data = data.slice(startIndex, data.length);
-    }
-
-    // const HIGH_PROP = 'close';
-    const HIGH_PROP = 'high';
+    const HIGH_PROP = 'close';
+    // const HIGH_PROP = 'high';
 
     const highest = Math.max.apply(this, data.map(d => d[HIGH_PROP]));
 
-    data = data.slice(-days);
+    let cutLoss = 0;
 
-    console.log(data, data.map(d => d.high - d.low).reduce((p, c) => p + c, 0) / days);
+    for (let i = days - 1; i < data.length; i += 1) {
+      const daysData = data.slice(i - days + 1, i + 1);
+      const dayData = data[i];
 
-    // 计算 ATR 规则如下
-    // TR=∣最高价-最低价∣和∣最高价-昨收∣和∣昨收-最低价∣的最大值
-    // 真实波幅（ATR）= TR 的 N 日简单移动平均
-    const sumTRs = data.reduce(
-      (prev, current, index) =>
-        Math.max(
-          current.high - current.low,
-          data[index - 1] ? Math.abs(current.high - data[index -1].close) : 0,
-          data[index - 1] ? Math.abs(current.low - data[index -1].close) : 0,
-        ) + prev,
-      0,
-    );
+      const sumTRs = daysData.reduce(
+        (prev, current, index) =>
+          Math.max(
+            current.high - current.low,
+            data[index - 1] ? Math.abs(current.high - data[index -1].close) : 0,
+            data[index - 1] ? Math.abs(current.low - data[index -1].close) : 0,
+          ) + prev,
+        0,
+      );
 
-    const ATR = sumTRs / days;
+      const ATR = sumTRs / days;
+
+      const dayCutLoss = dayData[HIGH_PROP] - ATR * 2;
+
+      cutLoss = Math.max(cutLoss, dayCutLoss);
+    }
 
     res.json({
       code: 0,
       data: {
-        ATR,
-        price: ATR * 2,
-        highest,
-        cutLoss: highest - ATR * 2,
+        cutLoss,
       }
     });
   } catch (e) {
